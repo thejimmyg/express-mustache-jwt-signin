6c3e3c7c8bf7099b6bd61a96072fe21dd81050a5
James Gardner <james@jimmyg.org>
2019-02-05 13:05:46 +0000

    Not committed earlier

--- README.md
+++ README.md
@@ -25,6 +25,105 @@ hello:
     admin: true
 ```
 
+## Using the Library
+
+Here's a fairly standard boilerplate for using this library together with express-mustache-overlays.
+
+Replace `express-edit-text` with your library name and change the `// Magic here` section to set up the middleware you are interested in.
+
+```
+const cookieParser = require('cookie-parser')
+const debug = require('debug')('express-edit-text')
+const { editText } = require('../lib/index.js')
+const express = require('express')
+const { overlaysOptionsFromEnv, overlaysDirsFromEnv, prepareMustacheOverlays, setupErrorHandlers } = require('express-mustache-overlays')
+const { makeStaticWithUser, setupMiddleware, signInOptionsFromEnv } = require('express-mustache-jwt-signin')
+
+const port = process.env.PORT || 80
+const overlaysOptions = overlaysOptionsFromEnv()
+const { scriptName } = overlaysOptions
+const { mustacheDirs, publicFilesDirs } = overlaysDirsFromEnv()
+const signInOptions = signInOptionsFromEnv(scriptName)
+const { signInUrl, signOutUrl } = signInOptions
+const secret = process.env.SECRET
+const editableDir = process.env.DIR
+if (!editableDir) {
+  throw new Error('No DIR environment variable set to specify the path of the editable files.')
+}
+const disableAuth = ((process.env.DISABLE_AUTH || 'false').toLowerCase() === 'true')
+if (!disableAuth) {
+  if (!secret || secret.length < 8) {
+    throw new Error('No SECRET environment variable set, or the SECRET is too short. Need 8 characters')
+  }
+  if (!signInUrl) {
+    throw new Error('No SIGN_IN_URL environment variable set')
+  }
+  if (!signOutUrl) {
+    throw new Error('No SIGN_OUT_URL environment variable set')
+  }
+} else {
+  debug('Disabled auth')
+}
+const disabledAuthUser = process.env.DISABLED_AUTH_USER
+const listTitle = process.env.LIST_TITLE || 'Edit Text Files'
+const editTitle = process.env.EDIT_TITLE || 'Editing'
+let validator = async (filename, content, editableDir) => {
+}
+if (process.env.VALIDATION_MODULE_PATH) {
+  debug(`Using the validator() async funtion exported as the 'validator' key from '${process.env.VALIDATION_MODULE_PATH}'`)
+  validator = require(process.env.VALIDATION_MODULE_PATH).validator
+}
+
+const main = async () => {
+  const app = express()
+  app.use(cookieParser())
+
+  const overlays = await prepareMustacheOverlays(app, overlaysOptions)
+
+  const authMiddleware = await setupMiddleware(app, secret, Object.assign({ overlays }, signInOptions))
+  const { signedIn, hasClaims } = authMiddleware
+  let { withUser } = authMiddleware
+  if (disableAuth) {
+    withUser = makeStaticWithUser(JSON.parse(disabledAuthUser || 'null'))
+  }
+  app.use(withUser)
+
+  // Magic here
+  const subApp = await editText(overlays, signedIn, hasClaims, editableDir, { validator, editTitle, listTitle })
+  app.use(scriptName, subApp)
+
+
+  // Set up any other overlays directories here
+  mustacheDirs.forEach(dir => {
+    debug('Adding mustache dir', dir)
+    overlays.overlayMustacheDir(dir)
+  })
+  publicFilesDirs.forEach(dir => {
+    debug('Adding publicFiles dir', dir)
+    overlays.overlayPublicFilesDir(dir)
+  })
+
+  await overlays.setup()
+
+  await setupErrorHandlers(app, { debug })
+
+  app.listen(port, () => console.log(`Example app listening on port ${port}`))
+}
+
+main()
+
+// Better handling of SIGINT and SIGTERM for docker
+process.on('SIGINT', function () {
+  console.log('Received SIGINT. Exiting ...')
+  process.exit()
+})
+
+process.on('SIGTERM', function () {
+  console.log('Received SIGTERM. Exiting ...')
+  process.exit()
+})
+```
+
 ## Example
 
 ```
@@ -197,7 +296,6 @@ Options:
 * `signedOutTitle` - e.g. `'Signed Out'`
 * `signInTitle` - e.g. `'Sign In'`
 * `signedOutTitle` - e.g. `'Signed Out'`
-
 
 ## Development
 
--- bin/server.js
+++ bin/server.js
@@ -5,17 +5,12 @@ const path = require('path')
 const { setupLogin, signInOptionsFromEnv, appOptionsFromEnv } = require('../lib')
 const { createCredentialsFromWatchedUsersYaml } = require('../lib/loadUsers')
 const { hashPassword } = require('../lib/hash')
-const { overlaysOptionsFromEnv, overlaysDirsFromEnv, prepareMustacheOverlays, setupErrorHandlers } = require('express-mustache-overlays')
+const { overlaysOptionsFromEnv, withOverlays, setupErrorHandlers } = require('express-mustache-overlays')
 
-const port = process.env.PORT || 80
 const usersYml = process.env.USERS_YML || path.join(__dirname, '..', 'yaml', 'users.yml')
 const stat = fs.statSync(usersYml)
 if (!(stat && stat.isFile())) {
   throw new Error(`No such users file '${usersYml}'. Please check USERS_YML.`)
-}
-const secret = process.env.SECRET
-if (!secret || secret.length < 8) {
-  throw new Error('No SECRET environment variable set, or the SECRET is too short. Need 8 characters')
 }
 
 // const credentials = {
@@ -36,88 +31,66 @@ if (!secret || secret.length < 8) {
 // Use the createCredentialsFromWatchedUsersYaml to specify users in a yaml file as we do here.
 
 const overlaysOptions = overlaysOptionsFromEnv()
-const { withPjaxPwa, scriptName } = overlaysOptions
-const { mustacheDirs, publicFilesDirs } = overlaysDirsFromEnv()
-
-// const scriptName = process.env.SCRIPT_NAME || ''
-// const publicUrlPath = process.env.PUBLIC_URL_PATH || scriptName + '/public'
-// const title = 'Express Mustache JWT Sign In'
-
-const signInOptions = signInOptionsFromEnv()
-const { signInUrl = scriptName + '/signin', signOutUrl = scriptName + '/signout' } = signInOptions
-const appOptions = appOptionsFromEnv(scriptName)
-const { dashboardUrl } = appOptions
+const { scriptName, port } = overlaysOptions
+const signInOptions = signInOptionsFromEnv( scriptName )
+const { dashboardUrl, adminUrl, hashUrl, secret } = signInOptions
 
 const main = async () => {
   const userData = await createCredentialsFromWatchedUsersYaml(usersYml)
   const app = express()
-  const overlays = await prepareMustacheOverlays(app, overlaysOptions)
 
-  const { withUser, signedIn, hasClaims } = await setupLogin(app, secret, userData.credentials, overlays, Object.assign({}, signInOptions, appOptions, { signInUrl, signOutUrl }))
-  app.use(withUser)
+  await withOverlays(app, overlaysOptions, async (overlays) => {
+    const { withUser, signedIn, hasClaims } = await setupLogin(app, userData.credentials, overlays, signInOptions)
+    app.use(withUser)
 
-  app.get(scriptName + '/', (req, res) => {
-    res.redirect(dashboardUrl)
-  })
+    app.get(scriptName + '/', (req, res) => {
+      res.redirect(dashboardUrl)
+    })
 
-  app.get(scriptName + '/dashboard', signedIn, async (req, res, next) => {
-    try {
-      res.render('content', { title: 'Dashboard', user: req.user, content: '<h1>Dashboard</h1><p>Not much to see here.</p>' })
-    } catch (e) {
-      debug(e)
-      next(e)
-    }
-  })
-
-  app.get(scriptName + '/admin', hasClaims(claims => claims.admin), async (req, res, next) => {
-    try {
-      res.render('content', { title: 'Admin', user: req.user, content: '<h1>Admin</h1><p>Only those with the <tt>admin</tt> claim set to <tt>true</tt> can see this.</p>' })
-    } catch (e) {
-      debug(e)
-      next(e)
-    }
-  })
-
-  app.all(scriptName + '/hash', hasClaims(claims => claims.admin), async (req, res, next) => {
-    try {
-      let hashError = ''
-      const action = req.path
-      let password = ''
-      let confirmPassword = ''
-      let hashed = ''
-      if (req.method === 'POST') {
-        password = req.body.password
-        confirmPassword = req.body.confirm_password
-        if (!password.length) {
-          hashError = 'Please enter a password'
-        } else if (password !== confirmPassword) {
-          hashError = 'Passwords must match'
-        } else {
-          hashed = await hashPassword(password)
-        }
+    app.get(dashboardUrl, signedIn, async (req, res, next) => {
+      try {
+        res.render('content', { title: 'Dashboard', user: req.user, content: '<h1>Dashboard</h1><p>Not much to see here.</p>' })
+      } catch (e) {
+        debug(e)
+        next(e)
       }
-      res.render('hash', { title: 'Hash', user: req.user, hashed, hashError, action })
-    } catch (e) {
-      debug(e)
-      next(e)
-    }
-  })
+    })
 
-  // Set up any other overlays directories here
-  mustacheDirs.forEach(dir => {
-    debug('Adding mustache dir:', dir)
-    overlays.overlayMustacheDir(dir)
-  })
-  publicFilesDirs.forEach(dir => {
-    debug('Adding public files dir:', dir)
-    overlays.overlayPublicFilesDir(dir)
-  })
+    app.get(adminUrl, hasClaims(claims => claims.admin), async (req, res, next) => {
+      try {
+        res.render('content', { title: 'Admin', user: req.user, content: '<h1>Admin</h1><p>Only those with the <tt>admin</tt> claim set to <tt>true</tt> can see this.</p>' })
+      } catch (e) {
+        debug(e)
+        next(e)
+      }
+    })
 
-  // Put the overlays into place after you've set up any more overlays you need, but definitely before the error handlers
-  await overlays.setup()
-
-  // Keep this right at the end, immediately before listening
-  await setupErrorHandlers(app, { debug })
+    app.all(hashUrl, hasClaims(claims => claims.admin), async (req, res, next) => {
+      try {
+        let hashError = ''
+        const action = req.path
+        let password = ''
+        let confirmPassword = ''
+        let hashed = ''
+        if (req.method === 'POST') {
+          password = req.body.password
+          confirmPassword = req.body.confirm_password
+          if (!password.length) {
+            hashError = 'Please enter a password'
+          } else if (password !== confirmPassword) {
+            hashError = 'Passwords must match'
+          } else {
+            hashed = await hashPassword(password)
+          }
+        }
+        res.render('hash', { title: 'Hash', user: req.user, hashed, hashError, action })
+      } catch (e) {
+        debug(e)
+        next(e)
+      }
+    })
+    await setupErrorHandlers(app, { debug })
+  })
 
   app.listen(port, () => console.log(`Example app listening on port ${port}`))
 }
